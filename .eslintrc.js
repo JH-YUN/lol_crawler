bafb05ce51c822a492056e9a0aac447b0e5d81b5
junhan <wnsgks789@gmail.com>
2019-12-13 20:33:45 +0900

    룬 크롤링 기능 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,6 @@ module.exports = {
     ecmaVersion: 2018,
   },
   rules: {
-    'no-console': false
+    'no-console':
   },
 };
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
+const request = require('request-promise-native');
 const opggUrl = 'https://www.op.gg/champion';
+
 
 
 async function get_position_spell(champion) {
@@ -15,7 +17,8 @@ async function get_position_spell(champion) {
   const spell = await page.$$eval('.champion-overview__table--summonerspell img.tip', el => el.map(el => /\/spell\/(.*)\.png/.exec(el.src)).map(el => el[1]));
   await browser.close();
 
-  await position.map(position => get_skill(champion, position));
+  await position.map(position => get_rune(champion, position));
+  // console.log(await(get_rune(champion, position)));
 
 
 }
@@ -82,21 +85,60 @@ async function get_skill(champion, position) {
 
 }
 
-async function get_rune(championId, position) {
-  // https://www.op.gg/champion/ajax/statistics/runeList/championId=13&position=MID
+async function get_rune(champion, position) {
+
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(opggUrl + '/' + champion + '/statistics/' + position + '/rune');
 
-  const runeHTML = await page.$eval(".champion-box-content", e => e.outHTML);
+  const runeHTML = await page.$eval(".champion-box-content", e => e.outerHTML);
   const runeCheerio = cheerio.load(runeHTML);
-  runeCheerio('.champion-stats__filter__item.tabHeader').not('.champion-stats__filter__item--all').each(function (i, e) {
+
+  let rune = new Array();
+  // let runeDetail = new Array();
+  await runeCheerio('.champion-stats__filter__item.tabHeader').not('.champion-stats__filter__item--all').each(function (i, e) {
     let obj = new Object();
 
-    obj.mainRune = runeCheerio(e).find('img').eq(1).src;
+    obj.mainRune = /\/perk.*\/(.*)\.png/.exec(runeCheerio(e).find('img').eq(1).attr('src'))[1];
+    obj.subRune = /\/perk.*\/(.*)\.png/.exec(runeCheerio(e).find('img').eq(2).attr('src'))[1];
+    obj.pick = runeCheerio(e).find('.champion-stats__filter__item__value b').eq(0).text();
+
+    rune.push(obj);
+  });
+
+  for (let e of rune) {
+    const url = get_rune_detail_url(13, position, e.mainRune, e.subRune);
+    const runeDetail = new Array();
+    await request({ url: url, transform: function (body) { return body } })
+      .then((body) => {
+        let detailCheerio = cheerio.load(body);
+        detailCheerio('.perk-page-wrap').each((i2, e2) => {
+          runeDetail[i2] = new Array();
+          detailCheerio(e2).find('div.perk-page__item--active img').each((i3, e3) => {
+            // console.log(/\/perk.*\/(.*)\.png/.exec(detailCheerio(e).attr('src'))[1]);
+            runeDetail[i2].push(/\/perk.*\/(.*)\.png/.exec(detailCheerio(e3).attr('src'))[1]);
+          })
+          detailCheerio(e2).find('.fragment-page div.perk-page__image img.active').each((i3, e3) => {
+            let src = detailCheerio(e3).attr('src');
+            runeDetail[i2].push(/\/perk.*\/(.*)\.png/.exec(src)[1]);
+          })
+
+        })
+        e.detail = runeDetail;
+      }).catch((err) => console.log(err));
   }
+  console.log(rune);
 
   await browser.close();
+
+  // return rune;
+
+
 }
+
+function get_rune_detail_url(championId, position, mainRune, subrune) {
+  return opggUrl + '/ajax/statistics/runeList/championId=' + championId + '&position=' + position + '&primaryPerkId=' + mainRune + '&subPerkStyleId=' + subrune;
+}
+
 
 get_position_spell('ryze');
